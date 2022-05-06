ffd688ab79957b739aac96ec258999545fff7073
BrenoM097 <melobreno89@gmail.com>
2022-05-06 18:34:01 -0300

    criando classes de modelos att

--- src/main/java/santander/bankline/api/model/Correntista.java
+++ src/main/java/santander/bankline/api/model/Correntista.java
@@ -1,5 +1,44 @@
 package santander.bankline.api.model;
 
 public class Correntista {
+	private Integer id;
+	private String cpf;
+	private String nome;
+	private Conta conta;
+	
+	// set e get dos private, o set sempre sendo um "void"
+	
+	public Conta getConta() {
+		return conta;
+	}
+	
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 
 }
